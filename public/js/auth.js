653efe3228543f4315234030271e27a0f0e0cc57
emerkaspar <emerkaspar@gmail.com>
2025-10-02 20:31:00 -0300

    area  DESLOGAD ok A

--- public/js/auth.js
+++ public/js/auth.js
@@ -8,12 +8,14 @@ const btnHeaderLoginGoogle = document.getElementById("btn-header-login-google");
 const userInfoDiv = document.getElementById("user-info");
 const appContent = document.getElementById("app-content");
 const welcomeSection = document.getElementById("welcome-section");
+const mainSummaryHeader = document.querySelector(".main-summary-header");
 
 const updateUIForLoggedInUser = (user) => {
     welcomeSection.style.display = "none";
     appContent.style.display = "block";
     userInfoDiv.style.display = "flex";
     btnHeaderLoginGoogle.style.display = "none";
+    mainSummaryHeader.style.display = "flex"; // Garante que o resumo apareça para usuários logados
     document.getElementById("user-photo").src = user.photoURL;
     document.getElementById("dropdown-user-name").textContent = user.displayName;
     document.getElementById("dropdown-user-email").textContent = user.email;
@@ -24,6 +26,7 @@ const updateUIForLoggedOutUser = () => {
     appContent.style.display = "none";
     userInfoDiv.style.display = "none";
     btnHeaderLoginGoogle.style.display = "flex";
+    mainSummaryHeader.style.display = "none"; // Esconde o resumo para usuários deslogados
 };
 
 // Função principal que inicializa a autenticação
